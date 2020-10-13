d2f32133d886972f5d40100664f21ffb6810f04d
RocketScience <stephanos@malikotsinas.de>
2020-10-13 19:49:41 +0200

    deffered css

---
+++ defercss.js
@@ -0,0 +1,14 @@
+var giftofspeed = document.createElement('link');
+giftofspeed.rel = 'stylesheet';
+giftofspeed.href = 'https://cdn3.devexpress.com/jslib/20.1.7/css/dx.common.css';
+giftofspeed.type = 'text/css';
+var godefer = document.getElementsByTagName('link')[0];
+godefer.parentNode.insertBefore(giftofspeed, godefer);
+
+/* Second CSS File */
+var giftofspeed2 = document.createElement('link');
+giftofspeed2.rel = 'stylesheet';
+giftofspeed2.href = 'https://cdn3.devexpress.com/jslib/20.1.7/css/dx.material.blue.light.css';
+giftofspeed2.type = 'text/css';
+var godefer2 = document.getElementsByTagName('link')[0];
+godefer2.parentNode.insertBefore(giftofspeed2, godefer2);
