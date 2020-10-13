edc9079354be79c5b57200415e461a4f5310a885
RocketScience <stephanos@malikotsinas.de>
2020-10-13 20:51:34 +0200

    minified css, removed double font loading

--- defercss.js
+++ defercss.js
@@ -1,6 +1,6 @@
 var giftofspeed = document.createElement('link');
 giftofspeed.rel = 'stylesheet';
-giftofspeed.href = 'https://cdn3.devexpress.com/jslib/20.1.7/css/dx.common.css';
+giftofspeed.href = 'dx.common.min.css';
 giftofspeed.type = 'text/css';
 var godefer = document.getElementsByTagName('link')[0];
 godefer.parentNode.insertBefore(giftofspeed, godefer);
@@ -8,7 +8,7 @@ godefer.parentNode.insertBefore(giftofspeed, godefer);
 /* Second CSS File */
 var giftofspeed2 = document.createElement('link');
 giftofspeed2.rel = 'stylesheet';
-giftofspeed2.href = 'https://cdn3.devexpress.com/jslib/20.1.7/css/dx.material.blue.light.css';
+giftofspeed2.href = 'dx.material.blue.light.min.css';
 giftofspeed2.type = 'text/css';
 var godefer2 = document.getElementsByTagName('link')[0];
 godefer2.parentNode.insertBefore(giftofspeed2, godefer2);
